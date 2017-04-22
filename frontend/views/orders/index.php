c317a7ace982cb2777d189353173a321e59e3cd9
John Doe <yarwalker@gmail.com>
2017-04-23 01:29:08 +0300

    repair index

--- frontend/views/orders/index.php
+++ frontend/views/orders/index.php
@@ -1,9 +1,54 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>orders/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\widgets\Pjax;
+use yii\widgets\ListView;
+use yii\bootstrap\Modal;
+
+?>
+<h3>Выбор блюд по ингридиентам</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+            <?php $form = ActiveForm::begin(['id' => 'order-form', 'enableAjaxValidation' => true]); ?>
+            <div class="form-group">
+                <?= Html::submitButton('Подобрать блюда', [
+                        'class' => 'btn btn-primary',
+                        'id' => 'choose-dish',
+                ]) ?>
+            </div>
+            <div class="info">Выбрано <span>0</span> ингридиент(а/ов)</div>
+
+            <div class="form-group ingridient-block">
+                <?= ListView::widget([
+                    'dataProvider' => $availableIngridientsProvider,
+                    'itemView' => '_view',
+                    'summary' => false,
+                ]); ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-lg-9">
+            <div id="chosen-dish">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php Modal::begin([
+        'header' => '<h2>Сообщение</h2>',
+        'headerOptions' => ['id' => 'modalMessageHeader'],
+        'id' => 'modalMessage',
+        'size' => 'modal-md',
+        //keeps from closing modal with esc key or by clicking out of the modal.
+        // user must click cancel or X to close
+        'clientOptions' => ['backdrop' => 'static', 'keyboard' => FALSE, ],
+    ]
+);
+
+Modal::end();
+?>
+
